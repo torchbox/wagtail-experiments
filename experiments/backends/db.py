2ec93e23ba4609845f5e0495076b69548d6a9478
Mikalai Radchuk <radchuk.m@gmail.com>
2016-10-18 20:14:01 +0300

    Add get_report method to the DB backend

--- experiments/backends/db.py
+++ experiments/backends/db.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import datetime
 
-from django.db.models import F
+from django.db.models import F, Sum
 
 from experiments.models import ExperimentHistory
 
@@ -23,3 +23,32 @@ def record_completion(experiment, user_id, variation):
     )
     # increment the completion_count
     ExperimentHistory.objects.filter(pk=history.pk).update(completion_count=F('completion_count') + 1)
+
+
+def get_report(experiment):
+    result = {}
+    result.setdefault('variations', [])
+
+    variations = experiment.get_variations()
+    for variation in variations:
+        history_entries = ExperimentHistory.objects.filter(experiment=experiment, variation=variation)
+
+        variation_data = {
+            'variation_pk': variation.pk,
+            'is_control': variation.pk == experiment.control_page.pk,
+            'is_winner': False,  # TODO: Needs real data
+            'total_participant_count': history_entries.aggregate(sum=Sum('participant_count')).get('sum', 0),
+            'total_completion_count': history_entries.aggregate(sum=Sum('completion_count')).get('sum', 0),
+        }
+        variation_data.setdefault('history', [])
+
+        for entry in history_entries:
+            variation_data['history'].append({
+                'date': entry.date,
+                'participant_count': entry.participant_count,
+                'completion_count': entry.completion_count,
+            })
+
+        result['variations'].append(variation_data)
+
+    return result
