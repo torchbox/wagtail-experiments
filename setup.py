0a1ab4b220ba8fc1b8c76c24951e737aec69a897
Matt Westcott <matt@west.co.tt>
2016-11-10 14:53:25 +0000

    Version bump to 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wagtail-experiments',
-    version='0.1.1',
+    version='0.1.2',
     description="A/B testing for Wagtail",
     author='Matthew Westcott',
     author_email='matthew.westcott@torchbox.com',
