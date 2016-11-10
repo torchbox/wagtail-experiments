bc599136c1144ab53b4f494147bdae54217cc810
Matt Westcott <matt@west.co.tt>
2016-11-10 13:46:58 +0000

    Add test fixtures for inline child objects

--- tests/models.py
+++ tests/models.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.wagtailcore.models import Page, Orderable
 
 
 class SimplePage(Page):
@@ -12,3 +13,9 @@ class SimplePage(Page):
         context = super(SimplePage, self).get_context(request)
         context['breadcrumb'] = self.get_ancestors(inclusive=True).filter(depth__gte=request.site.root_page.depth)
         return context
+
+
+class SimplePageRelatedLink(Orderable):
+    page = ParentalKey(SimplePage, related_name='related_links')
+    url = models.URLField()
+    link_text = models.CharField(max_length=255)
