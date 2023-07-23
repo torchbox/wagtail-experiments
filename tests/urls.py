ca42aa816bdffd4171a267b922b328cd92bf9446
TopDevPros <136071495+TopDevPros@users.noreply.github.com>
2023-07-23 17:41:27 +0000

    Update to support wagtail 5 and django 4.2

--- tests/urls.py
+++ tests/urls.py
@@ -1,23 +1,17 @@
-from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import include, url
-
-try:
-    from wagtail.admin import urls as wagtailadmin_urls
-    from wagtail.core import urls as wagtail_urls
-except ImportError:  # fallback for Wagtail <2.0
-    from wagtail.wagtailadmin import urls as wagtailadmin_urls
-    from wagtail.wagtailcore import urls as wagtail_urls
+from django.conf.urls import include, re_path
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
 
 from experiments import views as experiment_views
 
 
 urlpatterns = [
-    url(r'^admin/', include(wagtailadmin_urls)),
+    re_path(r'^admin/', include(wagtailadmin_urls)),
 
-    url(r'^experiments/complete/([^\/]+)/$', experiment_views.record_completion),
+    re_path(r'^experiments/complete/([^\/]+)/$', experiment_views.record_completion),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's serving mechanism
-    url(r'', include(wagtail_urls)),
+    re_path(r'', include(wagtail_urls)),
 ]
